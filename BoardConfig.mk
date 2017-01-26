1a6aca4d659ab90c89ac0a548804d7b6971f09b2
Sungwoo Park <swpark@nexell.co.kr>
2017-01-26 16:34:37 +0900

    Set TARGET_ARCH by TARGET_SOC
    This patch set TARGET_ARCH, TARGET_ARCH_VARIANT by TARGET_SOC.
    BoardConfig.mk includes selectively TargetArm64Config.mk or TargetArmConfig.mk
    by TARGET_SOC variable.
    
    Change-Id: I4689eded4ca9d32d9dc7ab7718e9c023a99870f5
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,17 +14,17 @@
 # limitations under the License.
 #
 
-TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
-TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a53
+ifeq ($(TARGET_SOC),s5p6818)
+-include device/nexell/avn_ref/TargetArm64Config.mk
+endif
 
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a53.a57
+ifeq ($(TARGET_SOC),s5p4418)
+-include device/nexell/avn_ref/TargetArmConfig.mk
+endif
+
+ifeq ($(TARGET_SOC),nxp4330)
+-include device/nexell/avn_ref/TargetArmConfig.mk
+endif
 
 # TODO: check below feature
 # ENABLE_CPUSETS := true
