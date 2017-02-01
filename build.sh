9dcabb2073b5dfecdcaccf411156922e8c70725c
Sungwoo Park <swpark@nexell.co.kr>
2017-02-01 20:31:21 +0900

    build: Add call gen_bl1, gen_third, post_process
    This patch adds calling gen_bl1, gen_third, post_process.
    After builds bl1, calls gen_bl1.
    After builds optee, calls gen_third and after all builds, call
    post_process to generate result dir.
    
    Change-Id: Ibf63da461c2b51ee23a029268e5554fc9aa5e024
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- build.sh
+++ build.sh
@@ -31,17 +31,41 @@ OPTEE_BUILD_OPT+=" UBOOT_DIR=${UBOOT_DIR}"
 if [ "${BUILD_ALL}" == "true" ] || [ "${BUILD_BL1}" == "true" ]; then
 	# TODO: after bl1 revision, apply real boardname to second arg
 	build_bl1 ${BL1_DIR}/bl1-${TARGET_SOC} AVN
+	gen_bl1 ${TARGET_SOC} ${BL1_DIR}/bl1-${TARGET_SOC}/out/bl1-avn.bin \
+		device/nexell/avn_ref/nsih_avn_ref_usb.txt \
+		${BL1_DIR}/bl1-${TARGET_SOC}/out/bl1-usbboot.img
+	gen_bl1 ${TARGET_SOC} ${BL1_DIR}/bl1-${TARGET_SOC}/out/bl1-avn.bin \
+		device/nexell/avn_ref/nsih_avn_ref_emmc.txt \
+		${BL1_DIR}/bl1-${TARGET_SOC}/out/bl1-emmcboot.img
 fi
 
 if [ "${BUILD_ALL}" == "true" ] || [ "${BUILD_UBOOT}" == "true" ]; then
 	build_uboot ${UBOOT_DIR} ${TARGET_SOC} ${BOARD} ${CROSS_COMPILE}
 	if [ "${BUILD_UBOOT}" == "true" ]; then
-		build_optee device/nexell/secure "${OPTEE_BUILD_OPT}" build-singleimage
+		build_optee ${OPTEE_DIR} "${OPTEE_BUILD_OPT}" build-fip-nonsecure
+		build_optee ${OPTEE_DIR} "${OPTEE_BUILD_OPT}" build-singleimage
+		# generate fip-nonsecure.img
+		gen_third ${TARGET_SOC} ${OPTEE_DIR}/optee_build/result/fip-nonsecure.bin \
+			device/nexell/avn_ref/nsih_avn_ref_emmc.txt \
+			0x7df00000 0x00000000 ${OPTEE_DIR}/optee_build/result/fip-nonsecure.img
 	fi
 fi
 
 if [ "${TARGET_SOC}" == "s5p6818" ] && [ "${BUILD_ALL}" == "true" ] || [ "${BUILD_SECURE}" == "true" ]; then
 	build_optee ${OPTEE_DIR} "${OPTEE_BUILD_OPT}" all
+	# generate fip-loader.img
+	gen_third ${TARGET_SOC} ${OPTEE_DIR}/optee_build/result/fip-loader.bin \
+		device/nexell/avn_ref/nsih_avn_ref_emmc.txt \
+		0x7fcc0000 0x7fd00800 ${OPTEE_DIR}/optee_build/result/fip-loader.img \
+		"-k 3 -m 0x60200 -b 3 -p 2 -m 0x1E0200 -b 3 -p 2"
+	# generate fip-secure.img
+	gen_third ${TARGET_SOC} ${OPTEE_DIR}/optee_build/result/fip-secure.bin \
+		device/nexell/avn_ref/nsih_avn_ref_emmc.txt \
+		0x7fb00000 0x00000000 ${OPTEE_DIR}/optee_build/result/fip-secure.img
+	# generate fip-nonsecure.img
+	gen_third ${TARGET_SOC} ${OPTEE_DIR}/optee_build/result/fip-nonsecure.bin \
+		device/nexell/avn_ref/nsih_avn_ref_emmc.txt \
+		0x7df00000 0x00000000 ${OPTEE_DIR}/optee_build/result/fip-nonsecure.img
 fi
 
 if [ "${BUILD_ALL}" == "true" ] || [ "${BUILD_KERNEL}" == "true" ]; then
@@ -51,3 +75,14 @@ fi
 if [ "${BUILD_ALL}" == "true" ] || [ "${BUILD_ANDROID}" == "true" ]; then
 	build_android ${TARGET_SOC} ${BOARD} userdebug
 fi
+
+post_process ${TARGET_SOC} \
+	device/nexell/${BOARD}/partmap.txt \
+	${RESULT_DIR} \
+	${BL1_DIR}/bl1-${TARGET_SOC}/out \
+	${OPTEE_DIR}/optee_build/result \
+	${UBOOT_DIR} \
+	${KERNEL_DIR}/arch/arm64/boot \
+	${KERNEL_DIR}/arch/arm64/boot/dts/nexell \
+	33554432 \
+	${TOP}/out/target/product/${BOARD}
