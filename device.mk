30d0154b9fafeed5ed436e0c36884ed0a2616188
Sungwoo Park <swpark@nexell.co.kr>
2017-03-13 16:21:54 +0900

    aosp_avn_ref: Moves auxilliary feature to device.mk
    This patch moves auxilliary feature to device.mk.
    
    Change-Id: I0a2b9f404ed2b9ef3701c096a4f552e8c51f3e2f
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

---
+++ device.mk
@@ -0,0 +1,104 @@
+#
+# Copyright (C) 2015 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_COPY_FILES += \
+	device/nexell/avn_ref/init.avn_ref.rc:root/init.avn_ref.rc \
+	device/nexell/avn_ref/init.avn_ref.usb.rc:root/init.avn_ref.usb.rc \
+	device/nexell/avn_ref/fstab.avn_ref:root/fstab.avn_ref \
+	device/nexell/avn_ref/ueventd.avn_ref.rc:root/ueventd.avn_ref.rc
+
+PRODUCT_COPY_FILES += \
+	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+
+# audio
+PRODUCT_COPY_FILES += \
+	frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:system/etc/a2dp_audio_policy_configuration.xml \
+	frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
+	frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml \
+	frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml
+
+# input
+PRODUCT_COPY_FILES += \
+	device/nexell/avn_ref/tsc2007.idc:system/usr/idc/tsc2007.idc \
+	device/nexell/avn_ref/keypad_avn_ref.kl:system/usr/keylayout/keypad_avn_ref.kl \
+	device/nexell/avn_ref/keypad_avn_ref.kcm:system/usr/keychars/keypad_avn_ref.kcm
+
+# hardware features
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+	frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+	frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
+PRODUCT_CHARACTERISTICS := tablet
+
+# OpenGL ES API version: 2.0
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.opengles.version=131072
+
+# density
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.sf.lcd_density=160
+
+PRODUCT_PACKAGES += \
+	audio.a2dp.default \
+	audio.usb.default \
+	audio.r_submix.default \
+	tinyplay
+
+# libion needed by gralloc, ogl
+PRODUCT_PACKAGES += libion iontest
+
+PRODUCT_PACKAGES += libdrm
+
+# HAL
+PRODUCT_PACKAGES += \
+	gralloc.avn_ref \
+	libGLES_mali \
+	hwcomposer.avn_ref
+
+PRODUCT_PACKAGES += fs_config_files
+
+# PRODUCT_PACKAGES += \
+	libtslib \
+	inputraw \
+	pthres \
+	dejitter \
+	linear \
+	tscalib \
+	TSCalibration
+
+# PRODUCT_COPY_FILES += \
+	external/tslib/ts.conf:system/etc/ts.conf
+
+DEVICE_PACKAGE_OVERLAYS := device/nexell/avn_ref/overlay
+
+# limit dex2oat threads to improve thermals
+PRODUCT_PROPERTY_OVERRIDES += \
+	dalvik.vm.boot-dex2oat-threads=4 \
+	dalvik.vm.dex2oat-threads=4 \
+	dalvik.vm.image-dex2oat-threads=4
+
+$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
